9bad4f5c3114131898f20e4c85e84c6fd7ec61b7
William Hurley <whurley@forumone.com>
2014-02-18 22:16:45 -0500

    Adding php_prefix option

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,24 @@
 define pear(
   $package, 
   $creates, 
-  $channel=""
+  $channel="",
+  $php_prefix
 ) {
 
   # Install the PEAR package.
-  if ! defined(Package['php-pear']) {
-    package { 'php-pear':
+  if ! defined(Package["${php_prefix}-pear"]) {
+    package { "${php_prefix}-pear":
       ensure => present,
     }
   }
   
   if $channel != "" and !defined(Exec["pear channel-discover ${channel}"]) {
-    $requires = [ Package['php-pear'], Exec["pear channel-discover ${channel}"]]
+    $requires = [ Package["${php_prefix}-pear"], Exec["pear channel-discover ${channel}"]]
     exec { "pear channel-discover ${channel}":
       # We can't execute channel-discover successfully if it exists already.
       command => "pear channel-info ${channel} || pear channel-discover ${channel}",
       creates => "/usr/share/php/.channels/${channel}.reg",
-      require => Package['php-pear'],
+      require => Package["${php_prefix}-pear"],
       path => ["/usr/bin", "/usr/sbin"],
     }
   }
